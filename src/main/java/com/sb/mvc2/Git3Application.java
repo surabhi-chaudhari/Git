cda214a07e10cdc377a5517ae2be54ebbe4bf541
surabhi <surabhichaudhari96@gmail.com>
2020-11-20 15:33:36 +0530

    initial commit

---
+++ src/main/java/com/sb/mvc2/Git3Application.java
@@ -0,0 +1,13 @@
+package com.sb.mvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/mvc2/Git3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.mvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
