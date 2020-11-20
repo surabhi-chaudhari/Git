9ab6a697878534e55ed584e73d691618c6fe9f27
surabhi <surabhichaudhari96@gmail.com>
2020-11-20 18:45:04 +0530

    f-200 changes

--- src/main/java/com/sb/mvc2/Git3Application.java
+++ src/main/java/com/sb/mvc2/Git3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Git3Application {
 
 	public static void main(String[] args) {
+                int y=22;
 		SpringApplication.run(Git3Application.class, args);
                     
                  int a=11;
